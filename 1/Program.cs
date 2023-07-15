78a786071aac7a49a58ce419b837a0aa2cb8e25b
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-07-15 23:51:29 +0200

    <

---
+++ 1/Program.cs
@@ -0,0 +1,21 @@
+﻿// using System;
+
+namespace ConsoleApp1
+{
+
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            bool canIVote = true;
+            
+            Console.WriteLine(canIVote);
+
+
+        }
+
+    }
+
+}
---
+++ enums/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace ConsoleApi
+{
+
+    public class Program
+    {
+
+        enum CarColor : byte
+        {
+            Orange = 1,
+            Blue,
+            Green,
+            Yellow
+
+        }
+
+        static void Main(string[] args) { }
+
+    }
+
+}
